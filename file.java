967613034989e5454d9111476a8a311a22b418c6
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-26 10:34:15 +0530

    Usecase 1

--- file.java
+++ file.java
@@ -2,6 +2,8 @@ public class file
 {
 public static void main(String args[])
 {
+int position =0;
 System.out.println("Welcome to Snake and Ladder");
+System.out.println("Current position : "+position);
 }
 }
