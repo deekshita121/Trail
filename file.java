e1e5554e9421539c8c13d3208cdfa75db4b61f15
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-26 11:06:05 +0530

    Usecase 2

--- file.java
+++ file.java
@@ -1,9 +1,13 @@
 public class file
 {
+public static final int position=0;
 public static void main(String args[])
 {
-int position =0;
 System.out.println("Welcome to Snake and Ladder");
-System.out.println("Current position : "+position);
+int die=(int)(Math.floor(Math.random()*10 % 6))+1;
+System.out.println("Dieroll : "+die);
+int current_position = position;
+current_position +=die;
+System.out.println("Current position : "+current_position);
 }
 }
