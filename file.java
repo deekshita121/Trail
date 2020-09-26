25576f843d8b2556ec5797f2eff8e1c1ec647d89
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-26 11:42:59 +0530

    Usecase 3

--- file.java
+++ file.java
@@ -1,13 +1,28 @@
 public class file
 {
-public static final int position=0;
+public static final int ladder=1;
+public static final int snake=2;
 public static void main(String args[])
 {
+int position=0;
 System.out.println("Welcome to Snake and Ladder");
 int die=(int)(Math.floor(Math.random()*10 % 6))+1;
 System.out.println("Dieroll : "+die);
-int current_position = position;
-current_position +=die;
-System.out.println("Current position : "+current_position);
+int option=(int)(Math.floor(Math.random()*10 % 3));
+switch(option)
+{
+case ladder:
+System.out.println("Ladder");
+position += die;
+break;
+case snake:
+System.out.println("Snake");
+position-= die;
+break;
+default:
+System.out.println("No play");
+position=position;
+}
+System.out.println("Position of player is "+position);
 }
 }
